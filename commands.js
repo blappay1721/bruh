e7bf7b43e4704cd92fda8ada10858fa044ad0e31
blappay1721 <blappay1721@gmail.com>
2025-04-25 20:14:18 +0000

    t

--- commands.js
+++ commands.js
@@ -1,21 +1,5 @@
 import 'dotenv/config';
-import { getRPSChoices } from './game.js';
 import { capitalize, InstallGlobalCommands } from './utils.js';
-
-// Get the game choices from game.js
-function createCommandChoices() {
-  const choices = getRPSChoices();
-  const commandChoices = [];
-
-  for (let choice of choices) {
-    commandChoices.push({
-      name: capitalize(choice),
-      value: choice.toLowerCase(),
-    });
-  }
-
-  return commandChoices;
-}
 
 // Simple test command
 const TEST_COMMAND = {
@@ -44,7 +28,15 @@ const PINGBOMB_COMMAND = {
 
 const STOPPING_COMMAND = {
   name: 'stopping',
-  description: 'Stop pinging a user',
+  description: 'Stop pinging a user (or all if admin)',
+  options: [
+    {
+      type: 6, // USER
+      name: 'user',
+      description: 'User whose pingbomb to stop (optional)',
+      required: false,
+    },
+  ],
   type: 1,
   integration_types: [0, 1],
   contexts: [0, 1, 2],
