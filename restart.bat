5d60c2b782da48940a67a7c2518935eb014e21fc
blappay1721 <blappay1721@gmail.com>
2025-04-26 13:09:52 -0700

    Delete restart.bat

--- restart.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-title Minecraft Server
-:loop
-echo Starting server...
-java -Xmx12G -Xms12G -jar paper.jar nogui
-echo Server crashed or closed! Restarting in 5 seconds...
-timeout /t 5
-goto loop
